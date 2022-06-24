b5d1db7ed1f2d0517b8d987eb1ab0b6827beb291
SymboLinker <SymboLinker@gmail.com>
2022-06-24 21:33:58 +0200

    Invoke .NET methods at events like `document.onkeydown`

---
+++ Examples/BlazorWebAssemblyAppWithKatex/DocumentEvents.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWebAssemblyAppWithKatex;
+
+public static class DocumentEvents
+{
+    public static Func<string, Task>? OnPhysicalKeyDownHandler { get; set; }
+    [JSInvokable]
+    public static async Task OnPhysicalKeyDown(string key)
+    {
+        Console.WriteLine("Hello from static method DOWN: " + key);
+        if (OnPhysicalKeyDownHandler is not null)
+        {
+            await OnPhysicalKeyDownHandler(key);
+        }
+    }
+    public static Func<string, Task>? OnPhysicalKeyUpHandler { get; set; }
+    [JSInvokable]
+    public static async Task OnPhysicalKeyUp(string key)
+    {
+        Console.WriteLine("Hello from static method UP: " + key);
+        if (OnPhysicalKeyUpHandler is not null)
+        {
+            await OnPhysicalKeyUpHandler(key);
+        }
+    }
+}
