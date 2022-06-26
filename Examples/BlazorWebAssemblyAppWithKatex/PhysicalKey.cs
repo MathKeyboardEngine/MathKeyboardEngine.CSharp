dd8a2dc5355f5c33645778b5caebd73076e99abe
SymboLinker <SymboLinker@gmail.com>
2022-06-26 05:58:04 +0200

    Physical keydown handling

---
+++ Examples/BlazorWebAssemblyAppWithKatex/PhysicalKey.cs
@@ -0,0 +1,31 @@
+﻿using MathKeyboardEngine;
+
+namespace BlazorWebAssemblyAppWithKatex;
+
+public class PhysicalKeyHandler
+{
+    private readonly Func<string, bool> _keyPredicate;
+    private readonly Action<KeyboardMemory, string> _actionForKeyboardMemoryAndKey;
+
+    public PhysicalKeyHandler(string key, Action<KeyboardMemory, string> actionForKeyboardMemoryAndKey)
+    {
+        _keyPredicate = (inputKey) => inputKey == key;
+        _actionForKeyboardMemoryAndKey = actionForKeyboardMemoryAndKey;
+    }
+
+    public PhysicalKeyHandler(Func<string, bool> keyPredicate, Action<KeyboardMemory, string> actionForKeyboardMemoryAndKey)
+    {
+        _keyPredicate = keyPredicate;
+        _actionForKeyboardMemoryAndKey = actionForKeyboardMemoryAndKey;
+    }
+
+    public bool CanHandle(string key)
+    {
+        return _keyPredicate(key);
+    }
+
+    public void Handle(KeyboardMemory k, string key)
+    {
+        _actionForKeyboardMemoryAndKey(k, key);
+    }
+}
